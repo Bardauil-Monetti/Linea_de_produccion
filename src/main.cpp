f7bea058618a78c042eab7e8e31bd521145039e4
Bardauil-Monetti <gbardauil548@gmail.com>
2025-11-22 20:53:25 -0300

    aa

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,88 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int sensores[4] = {1, 2, 3, 4};
+int pinBot = 12;
+int pinesDisplay[7] = {5, 6, 7, 8, 9, 10, 11};
+
+int display[6][7] = {
+  {0,1,1,0,0,0,0},
+  {1,1,0,1,1,0,1},
+  {1,1,1,1,0,0,1},
+  {0,1,1,0,0,1,1},
+  {0,0,0,1,1,1,1},
+  {0,0,0,0,0,0,1}
+};
+
+bool finished = false, estado = true;
+
+int tActual, tPrevio, tDelay = 10000;
+int tActual2, tPrevio2, tDelay2 = 500;
+
+volatile bool flag[4] = {false, false, false, false};
+volatile bool boton = false;
+
+void IRAM_ATTR ISR0() { flag[0] = true; }
+void IRAM_ATTR ISR1() { flag[1] = true; }
+void IRAM_ATTR ISR2() { flag[2] = true; }
+void IRAM_ATTR ISR3() { flag[3] = true; }
+
+
+void IRAM_ATTR ISR_Bot() { boton = true; }
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  for (int i = 0; i < 4; i++) {
+    pinMode(sensores[i], INPUT_PULLUP);
+  }
+  attachInterrupt(digitalPinToInterrupt(sensores[0]), ISR0, FALLING);
+  attachInterrupt(digitalPinToInterrupt(sensores[1]), ISR1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(sensores[2]), ISR2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(sensores[3]), ISR3, FALLING);
+
+  for (int i = 0; i < 7; i++) {
+    pinMode(pinesDisplay[i], OUTPUT);
+  }
+
+  pinMode(pinBot, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinBot), ISR_Bot, FALLING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for(int i = 0; i<4; i++){
+    if(flag[i]){
+      for(int j = 0; j < 7; j++){
+        digitalWrite(pinesDisplay[j], display[i][j]);
+      }
+      flag[i] = false;
+    }
+    if(flag[3]) {
+      finished = true;
+      tPrevio = millis();
+    }
+  }
+  tActual = millis();
+  if(finished && tActual-tPrevio >= tDelay){
+    if(!boton){
+      tActual2 = millis();
+      if(tActual2-tPrevio2 >= tDelay2){
+        if(estado) estado = false;
+        else estado = true;
+        tPrevio2 = tActual2;
+      }
+      if(estado){
+        for(int i = 0; i < 7; i++){
+          digitalWrite(pinesDisplay[i], display[4][i]);
+        }
+      }else{
+        for(int i = 0; i < 7; i++){
+          digitalWrite(pinesDisplay[i], LOW);
+        }
+      }
+    }else{
+      for(int i = 0; i < 7; i++){
+        digitalWrite(pinesDisplay[i], display[5][i]);
+      }
+      finished = false;
+      boton = false;
+    }
+  }
 }
